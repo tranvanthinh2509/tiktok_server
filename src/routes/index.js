8f8893f9c2cc88b5c5d206e3528c31a688c2ac3a
tranvanthinh <badagor999@gmail.com>
2024-05-28 20:11:54 +0700

    update vercel

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,12 @@ const routes = (app) => {
   app.use("/api/user", UserRouter);
   app.use("/api/video", VideoRouter);
   app.use("/api/comment", CommentRouter);
+  app.use("/api/test", async (req, res) => {
+    return res.json({
+      status: "Ok",
+      message: "success",
+    });
+  });
 };
 
 module.exports = routes;
