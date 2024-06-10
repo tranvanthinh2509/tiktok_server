f8e09c1c7c01adeec4270cbf400bfd9eb770710d
tranvanthinh <badagor999@gmail.com>
2024-06-10 11:33:31 +0700

    vercel 2

--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
 const express = require("express");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const { default: mongoose } = require("mongoose");
 const routes = require("./routes");
-const cors = require("cors")
+const cors = require("cors");
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser')
-dotenv.config()
+const cookieParser = require("cookie-parser");
+dotenv.config();
 
-const app = express()
-const port = process.env.PORT || 3001
- 
-app.use(cors())
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
-app.use(bodyParser.json())
-app.use(cookieParser())
+const app = express();
+const port = process.env.PORT || 3001;
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["POST", "PUT", "GET", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 routes(app);
-mongoose.connect(`${process.env.Mongo_DB}`)
-    .then(() => {
-        console.log('Connect db success')
-    })
-    .catch((err) => {
-        console.log('Connect db that bai')
-        console.log(err)
-    })
-
+mongoose
+  .connect(`${process.env.Mongo_DB}`)
+  .then(() => {
+    console.log("Connect db success");
+  })
+  .catch((err) => {
+    console.log("Connect db that bai");
+    console.log(err);
+  });
 
 app.listen(port, () => {
-    console.log('Server port: localhost:' + port)
-})
+  console.log("Server port: localhost:" + port);
+});
